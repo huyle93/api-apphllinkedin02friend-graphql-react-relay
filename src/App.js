e0904fd5a03bf6cd33ae6c8d1f5f85380ee36cf1
huyle93 <huyle.finance@gmail.com>
2018-04-05 20:48:53 -0400

    setting up React and Relay

--- src/App.js
+++ src/App.js
@@ -1,1 +1,54 @@
-// completed graphql relay backend before adding react
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryRenderer, graphql } from 'react-relay';
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+
+import Friends from './components/Friends';
+
+// Network layer to fetch data from graphQL
+function fetchQuery(operation, variables) {
+    return fetch('/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: operation.text,
+            variables,
+        }),
+    }).then(response => {
+        return response.json()
+    });
+}
+
+// Relay environment
+// Relay allows React components to integrate seamlessly with GraphQL
+const modernEnvironment = new Environment({
+    network: Network.create(fetchQuery),
+    store: new Store(new RecordSource()),
+});
+
+const mountNode = document.getElementById('root');
+
+ReactDOM.render(
+    <QueryRenderer
+        environment = {modernEnvironment}
+        {/* QUERY */}
+        query = {graphql`
+            query AppQuery {
+                viewer {
+                    ...Friends_viewer
+                }
+            }
+        `}
+        variables={{}}
+        render={({error, props}) => {
+            if (props) {
+                return <Friends viewer={props.viewer} />;
+            } else {
+                return <div>Loading...</div>
+            }
+        }}
+    />,
+    mountNode
+);
